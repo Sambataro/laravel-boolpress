dcd6cd21fe613e5b7a9d15c60a72b7b319ac1af4
fortunato <sambataro1990@gmail.com>
2021-02-18 16:09:51 +0100

    creato relazione tra posts e infopost

---
+++ database/migrations/2021_02_18_131207_create_info_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInfoPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('info_posts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('post_id');
+            $table->string('post_status', 8);
+            $table->string('comment_status', 8);
+            // $table->timestamps();
+
+            // DB relation
+            $table->foreign('post_id')
+                  ->references('id')
+                  ->on('posts');  
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('info_posts');
+    }
+}
